f968158ef0ff46f70d1d1eb8c0ff1501b7a4394b
IsaquePemasi <ADSisaque@gmail.com>
2022-10-01 17:46:51 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import CoinList from './components/CoinList';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <CoinList />
     </div>
   );
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <p>Home</p>
+        <p>Contato</p>
+        <p>Compra</p>
+
+    </div>
+  )
+}
