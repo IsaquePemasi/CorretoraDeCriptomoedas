25ddf896745b6089c46dbaac6ebfbfb9c461dd13
IsaquePemasi <ADSisaque@gmail.com>
2022-10-03 18:58:59 -0300

    rev footer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div>
-        <p>Home</p>
+       <p>Home</p>
         <p>Contato</p>
         <p>Compra</p>
         <p>Cotação</p>
