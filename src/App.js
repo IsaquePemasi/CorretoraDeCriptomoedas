314d6bf5f0839369df892bd20e0ef56d36bbd1cb
IsaquePemasi <ADSisaque@gmail.com>
2023-01-14 17:55:16 -0300

    rev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import CoinList from './components/CoinList';
-import { Header } from './components/Header';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Contato from "./pages/Contato"
 import Compra from "./pages/Compra"
 
 
 function App() {
   return (
     <div className='App'>
-      
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />}> </Route>
-          <Route path="/Contato" element={ <Contato /> }/>
-          <Route path="/Compra" element={ <Compra /> }/>
-        </Routes>
-      </BrowserRouter>
+
+    <Compra/>
+
     </div>
   );
 }
